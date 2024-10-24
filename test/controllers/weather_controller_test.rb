bf2a70785e48557a803a7b4fc78c7355bc037feb
Corey Fleming <coreypfleminghcp@gmail.com>
2024-10-24 13:35:59 -0400

    Update spec to handle invalid input

--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -16,5 +16,17 @@ class WeatherControllerTest < ActionDispatch::IntegrationTest
     assert @response.body.include? "Zipcode"
     assert @response.body.include? "Cached Results"
     assert @response.body.include? "Search"
+    assert_not @response.body.include? "Sorry"
+  end
+
+  test "should handle invalid zipcode" do
+    get weather_results_url, params: { zipcode: "aaaaa" }
+    assert_response :success
+    assert @response.body.include? "Sorry"
+    assert_not @response.body.include? "Current Temperature"
+    assert_not @response.body.include? "Max Temperature"
+    assert_not @response.body.include? "Min Temperature"
+    assert_not @response.body.include? "Cached Results"
+    assert @response.body.include? "Search"
   end
 end
