f9575c8e8ab2e7cab96bf3563a18730dfd98be90
Corey Fleming <coreypfleminghcp@gmail.com>
2024-10-24 11:47:48 -0400

    Update UI

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,6 +4,7 @@ class WeatherController < ApplicationController
 
   def results
     @cache_exists = Rails.cache.exist?(zipcode_param)
+    @zipcode_param = zipcode_param
     @weather_results = Rails.cache.fetch(zipcode_param, expires_in: 30.minutes) do
       weather_client.current_zip(zipcode_param)
     end
